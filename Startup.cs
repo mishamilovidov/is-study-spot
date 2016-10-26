3ffd78df2b2884992abe951f86919a61eae07c7e
Misha Milovidov <mmilovidov@doterra.com>
2016-10-25 21:43:32 -0600

    Editing initial setup

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+// using ISStudySpot.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
 
 namespace ISStudySpot
 {
@@ -27,6 +32,9 @@ namespace ISStudySpot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // var connection = "Data Source=sqlsv-instance1.ce61i890rwzw.us-west-2.rds.amazonaws.com,1433; Initial Catalog=ISStudySpot; Persist Security Info=True; User ID=sqlsv_i1_admin; Password=goKCaG86rsKVhtET3;";
+        	// services.AddDbContext<ISStudySpotContext>(options => options.UseSqlServer(connection));
+
             // Add framework services.
             services.AddMvc();
         }
