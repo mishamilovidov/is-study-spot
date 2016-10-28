430a1516bec6d559032f841657a1bbaed159fc09
Misha Milovidov <mmilovidov@doterra.com>
2016-10-27 21:42:41 -0600

    Study Resources view

--- Controllers/ClassesController.cs
+++ Controllers/ClassesController.cs
@@ -23,25 +23,18 @@ namespace ISStudySpot.Controllers
             return View();
         }
 
-        public IActionResult Class(int? id)
+        public IActionResult studyresources(int? id)
         {
             IEnumerable<ClassInformation> classinformation = _context.ClassInformation.FromSql(
-            "SELECT Classes.ClassID, Subjects.SubjectCode, Subjects.SubjectName, Subjects.SubjectDescription, " +
-            "Class_Note_Sections.ClassNoteSectionID,Class_Note_Sections.ClassNoteSectionTitle, Class_Note_Sections.ClassNoteSectionCreateDate, " +
-            "Class_Note_Sections.ClassNoteSectionUpdateDate, Class_Note_Section_Types.ClassNoteSectionTypeID, " +
-            "Class_Note_Section_Types.ClassNoteSectionTypeDescription, Class_Notes.ClassNoteID, Class_Notes.ClassNoteCreateDate, " +
-            "Class_Notes.ClassNoteUpdateDate, Class_Notes.ClassNoteTitle, Class_Notes.ClassNoteText, " +
-            "Class_Note_Comments.ClassNoteCommentCreateDate, Class_Note_Comments.ClassNoteCommentUpdateDate, " +
-            "Class_Note_Comments.ClassNoteCommentTitle, Class_Note_Comments.ClassNoteCommentText, Students.StudFirstName, Students.StudLastName, " +
-            "Teachers.TeachFirstName, Teachers.TeachLastname " +
-            "FROM Classes " +
-            "INNER JOIN Subjects ON Subjects.SubjectID = Classes.SubjectID " +
+            "SELECT newId() AS ColId, Classes.ClassID, Class_Note_Sections.ClassNoteSectionID, Class_Note_Sections.ClassNoteSectionTitle, Class_Note_Sections.ClassNoteSectionCreateDate, Class_Note_Sections.ClassNoteSectionUpdateDate, Class_Note_Section_Types.ClassNoteSectionTypeID, Class_Note_Section_Types.ClassNoteSectionTypeDescription, Class_Notes.ClassNoteID, Class_Notes.ClassNoteCreateDate, Class_Notes.ClassNoteUpdateDate, Class_Notes.ClassNoteTitle, Class_Notes.ClassNoteText, Students.StudFirstName, Students.StudLastName, Teachers.TeachFirstName, Teachers.TeachLastname, Subjects.SubjectCode, Subjects.SubjectName, Subjects.SubjectDescription, Semesters.SemesterName, Semesters.SemesterStartDate, Semesters.SemesterEndDate " +
+            "FROM Subjects " +
+            "INNER JOIN Classes ON Subjects.SubjectID = Classes.SubjectID " +
             "INNER JOIN Class_Note_Sections ON Class_Note_Sections.ClassID = Classes.ClassID " +
             "INNER JOIN Class_Note_Section_Types ON Class_Note_Section_Types.ClassNoteSectionTypeID = Class_Note_Sections.ClassNoteSectionTypeID " +
             "INNER JOIN Class_Notes ON Class_Notes.ClassNoteSectionID = Class_Note_Sections.ClassNoteSectionID " +
-            "INNER JOIN Class_Note_Comments ON Class_Note_Comments.ClassNoteID = Class_Notes.ClassNoteID " +
             "INNER JOIN Teachers ON Teachers.TeacherID = Class_Notes.TeacherID " +
             "INNER JOIN Students ON Students.StudentID = Class_Notes.StudentID " +
+            "INNER JOIN Semesters ON Semesters.SemesterID = Classes.SemesterID " +
             "WHERE Classes.ClassID = " + id
             );
 
--- Models/ClassInformation.cs
+++ Models/ClassInformation.cs
@@ -7,28 +7,28 @@ namespace ISStudySpot.Models
     public partial class ClassInformation
     {
         [Key]
+        public Guid ColId { get; set; }
+        public int ClassNoteSectionId { get; set; }
         public int ClassId { get; set; }
         public string SubjectCode { get; set; }
         public string SubjectName { get; set; }
         public string SubjectDescription { get; set; }
-        public int ClassNoteSectionId { get; set; }
         public string ClassNoteSectionTitle { get; set; }
         public DateTime? ClassNoteSectionCreateDate { get; set; }
-        public DateTime? ClassNoteSectionUpdateDate { get; set; }
+        public byte[] ClassNoteSectionUpdateDate { get; set; }
         public int ClassNoteSectionTypeId { get; set; }
-        public int ClassNoteSectionTypeDescription { get; set; }
+        public string ClassNoteSectionTypeDescription { get; set; }
         public int ClassNoteId { get; set; }
         public DateTime? ClassNoteCreateDate { get; set; }
-        public DateTime? ClassNoteUpdateDate { get; set; }
+        public byte[] ClassNoteUpdateDate { get; set; }
         public string ClassNoteTitle { get; set; }
         public string ClassNoteText { get; set; }
-        public DateTime? ClassNoteCommentCreateDate { get; set; }
-        public DateTime? ClassNoteCommentUpdateDate { get; set; }
-        public string ClassNoteCommentTitle { get; set; }
-        public string ClassNoteCommentText { get; set; }
         public string StudFirstName { get; set; }
         public string StudLastName { get; set; }
         public string TeachFirstName { get; set; }
         public string TeachLastname { get; set; }
+        public string SemesterName { get; set; }
+        public DateTime? SemesterStartDate { get; set; }
+        public DateTime? SemesterEndDate { get; set; }
     }
 }
